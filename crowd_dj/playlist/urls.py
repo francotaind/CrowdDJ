1c0fe98cfe181366b6e1753735724d5ef51dce7f
francotaind <francotaind00@gmail.com>
2024-07-09 07:20:36 +0300

    minor fixes

--- crowd_dj/playlist/urls.py
+++ crowd_dj/playlist/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('party/', views.party_room, name='party_room'),
     ]
--- crowd_dj/playlist/views.py
+++ crowd_dj/playlist/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def party_room(request):
+    return render(request, 'party_room.html')
